2c8c9df8f8f4783dc1468a948222a93e32b92858
haider.nasir6462 <haider.nasir6462@gmail.com>
2021-09-12 12:54:33 +0000

    buttons and dots

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -1,36 +1,94 @@
-import Carousel from 'react-elastic-carousel';
-import React from 'react'
+import React, { Component } from "react";
+import Slider from "react-slick";
 import Data from './cars.json'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-class Haider extends React.Component {
-  constructor(props) {
-    super(props)
-    this.breakPoints = [
-      { width: 1, itemsToShow: 1 },
-      { width: 550, itemsToShow: 2, itemsToScroll: 1, pagination: false },
-      { width: 850, itemsToShow: 4 },
-      { width: 1150, itemsToShow: 4, itemsToScroll: 1 },
-      { width: 1450, itemsToShow: 5 },
-      { width: 1750, itemsToShow: 6 },
-    ]
-  }
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "black", right: 10, zIndex: 999 }}
+      onClick={onClick}
+      />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "black", left: 10, zIndex: 999 }}
+      onClick={onClick}
+    />
+  );
+}
+export default class Responsive extends Component {
   render() {
-    return (
-      <Carousel breakPoints={this.breakPoints}>
-        {Data.map( post =>{
-          return(
-            <div className="m-3">
-              <p class="card-text">{post.bodyType}</p>
-              <div className="d-flex flex-xl-row flex-md-column">
-                <h5 class="card-title me-2">{post.modelName}</h5>
-                <p class="card-text">{post.modelType}</p>
-              </div>
-              <img src={post.imageUrl} class="card-img-top" alt="..."/>
-            </div>
-          )})
+    var settings = {
+      arrows: true,
+      dots: false,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      initialSlide: 0,
+      prevArrow: <SamplePrevArrow />,
+      nextArrow: <SampleNextArrow />,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            initialSlide: 2,
+            dots: true,
+            prevArrow: '',
+            nextArrow: '',
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            prevArrow: '',
+            nextArrow: '',
+          }
         }
-      </Carousel>
-    )
+      ]
+    };
+    return (
+      <div>
+        <Slider {...settings} className="mx-1">
+        {Data.map( post =>{
+            return(
+              <div className="m-3">
+                <p className="card-text">{post.bodyType}</p>
+                <div className="d-flex flex-xl-row flex-md-column">
+                  <h5 className="card-title me-2">{post.modelName}</h5>
+                  <p className="card-text">{post.modelType}</p>
+                </div>
+                <img src={post.imageUrl} className="card-img-top p-1" alt="..."/>
+              </div>
+            )})
+          }
+        </Slider>
+      </div>
+    );
   }
 }
-export default Haider
+
